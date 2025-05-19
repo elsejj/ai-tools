e7cb915cfafcd775875a57ce5f474c9513c46252
elsejj <lijia.c@gmail.com>
2025-05-19 19:38:55 +0800

    start it when llm-gateway exists

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,12 +1,42 @@
 import { app, BrowserWindow, ipcMain, globalShortcut, } from 'electron'
-import { electronApp, optimizer } from '@electron-toolkit/utils'
+import { electronApp} from '@electron-toolkit/utils'
 import { WindowManager } from './manager/windowManager'
 import { sendKeys } from 'sendkey'
-// import { join } from 'path'
-// import { spawn, ChildProcessWithoutNullStreams  } from 'child_process'
+import { join } from 'path'
+import { spawn, ChildProcessWithoutNullStreams  } from 'child_process'
+import { existsSync } from 'fs'
 
 
-//let gatewayProcess: ChildProcessWithoutNullStreams | null = null
+let gatewayProcess: ChildProcessWithoutNullStreams | null = null
+
+function restartGateway() {
+  stopGateway()
+
+  const llmGateway = join(app.getAppPath(), 'llm-gateway', 'llm_gateway.js')
+  if (!existsSync(llmGateway)) {
+    console.warn('llm-gateway not found, please check the path:', llmGateway)
+    return
+  }
+  const exePath = app.getPath('exe')
+  const llmGatewayArgs = [llmGateway, "--port=30027", "--headless"]
+  console.log('llmGateway:', exePath, llmGatewayArgs)
+
+  gatewayProcess = spawn(exePath, llmGatewayArgs , {
+    env: {
+      "LLM_GATEWAY_KEY_STORE_FILE" : join(app.getPath('userData'), 'llmkeys.json'),
+      "ELECTRON_RUN_AS_NODE": "1",
+    },
+    detached: true,
+  });
+}
+
+function stopGateway() {
+  if (gatewayProcess) {
+    gatewayProcess.kill()
+    console.log('gatewayProcess killed', gatewayProcess.killed)
+    gatewayProcess = null
+  }
+}
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -15,19 +45,9 @@ app.whenReady().then(() => {
   // Set app user model id for windows
   electronApp.setAppUserModelId('com.ai-tools')
 
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
-  app.on('browser-window-created', (_, window) => {
-    optimizer.watchWindowShortcuts(window)
-  })
-
-
-  // IPC test
-  ipcMain.on('ping', () => console.log('pong'))
-
   WindowManager.getInstance().createWindow()
 
+  // register global shortcuts Ctrl+Q
   globalShortcut.register('CommandOrControl+Q', () => {
     const mainWindow = WindowManager.getInstance().mainWindow
     if (mainWindow) {
@@ -41,19 +61,9 @@ app.whenReady().then(() => {
     }
   })
 
-
-  // const llmGateway = join(app.getAppPath(), 'llm-gateway', 'llm_gateway.js')
-  // const exePath = app.getPath('exe')
-  // const llmGatewayArgs = [llmGateway, "--port=30027", "--headless"]
-  // console.log('llmGateway:', exePath, llmGatewayArgs)
-
-  // gatewayProcess = spawn(exePath, llmGatewayArgs , {
-  //   env: {
-  //    "LLM_GATEWAY_KEY_STORE_FILE" : join(app.getPath('userData'), 'llmkeys.json'), 
-  //    "ELECTRON_RUN_AS_NODE": "1",
-  //   },
-  //   detached: true,
-  // });
+  // start the gateway and listen for the restart event
+  restartGateway()
+  ipcMain.on('restartGateway', restartGateway)
 
 
   app.on('activate', function () {
@@ -69,11 +79,7 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
-    // if (gatewayProcess) {
-    //   gatewayProcess.kill()
-    //   console.log('gatewayProcess killed', gatewayProcess.killed)
-    //   gatewayProcess = null
-    // }
+    stopGateway()
   }
 })
 
